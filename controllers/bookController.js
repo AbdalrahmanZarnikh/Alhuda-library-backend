75d871f28793801768be6b8b0ae162045efcb72d
AbdAlrahman13 <blalabo89@gmail.com>
2025-10-13 11:07:01 +0300

    commit

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -43,14 +43,25 @@ const createBook = asyncHandler(async (req, res) => {
     req.body.images = req.images;
   }
 
-  const existBook = await BookModel.findOne({ title: req.body.title ,category:req.body.category });
+  const existBook = await BookModel.findOne({
+    title: req.body.title,
+    category: req.body.category,
+  });
 
   if (existBook) {
     existBook.quantity += +req.body.quantity;
-    existBook.number= existBook.number+","+req.body.number;
-    await existBook.save()
+
+    if (req.body.number) {
+      existBook.number = existBook.number
+        ? existBook.number + "," + req.body.number
+        : req.body.number;
+    }
+
+    await existBook.save();
     res.status(201).json({ status: "Success", data: existBook });
   } else {
+    // تأكد من وجود number عند الإنشاء
+    if (!req.body.number) req.body.number = "";
     const book = await BookModel.create(req.body);
     res.status(201).json({ status: "Success", data: book });
   }
